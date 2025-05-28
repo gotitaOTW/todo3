6289d2557d0aa8f7e930490364d206e84619c74f
THIAGO MARTIN GARCIA SALMASO <48112397@ort>
2025-05-28 10:39:22 -0300

    apuntes pre codear

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+//import { useState } from 'react'
+//git clone https://github.com/gotitaOTW/todo2-EFSI.git
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
